82624e829d7a834dcb29cafdab614257be5dce18
Pine Wu <t-zhwu@microsoft.com>
2016-08-31 15:37:35 -0700

    Clean up code

--- src/extension.ts
+++ src/extension.ts
@@ -8,44 +8,128 @@ import * as path from 'path';
 import * as jmespath from 'jmespath';
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand('extension.initJmespath', () => {
-        var editor = vscode.window.activeTextEditor;
-        var selection = editor.selection;
-        var originalJson = editor.document.getText(selection);
-        
-        if (!originalJson)
-            return;
-
-        var tmpFileUri = path.join(os.tmpdir(), 'vscode-jmespath-preview.json');
-        fs.writeFileSync(tmpFileUri, JSON.stringify(JSON.parse(originalJson), null, 2));
-        vscode.commands.executeCommand('vscode.open', Uri.file(tmpFileUri), getViewColumn()).then(() => {
-            function liveUpdateDoc(pattern: string) {
-                var selectedJson = jmespath.search(JSON.parse(originalJson), pattern);
-                fs.writeFileSync(tmpFileUri, JSON.stringify(selectedJson, null, 2));
-
-                return null;
-            }
-
-            vscode.window.showInputBox({ validateInput: liveUpdateDoc }).then((pattern) => {});
-        });
-    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.runJmespath', runJmespath));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.runJmespathInPlace', runJmespathInPlace));
 }
 
 export function deactivate() {
 }
 
+function runJmespath() {
+    const selectedJson = getSelectedJson();
+
+    if (!selectedJson) {
+        vscode.window.showInformationMessage('Selected string is not a valid JSON');
+        return;
+    }
+
+    const tmpFilePath = path.join(os.tmpdir(), 'vscode-jmespath-preview.json');
+    const tmpFileUri = Uri.file(tmpFilePath);
+
+    fs.writeFileSync(tmpFilePath, stringifyJson(selectedJson, 2));
+
+    vscode.commands.executeCommand('vscode.open', tmpFileUri, getViewColumn()).then(() => {
+        function liveUpdateOutput(pattern: string) {
+            let outputJson;
+            try {
+                outputJson = jmespath.search(selectedJson, pattern);
+            } catch (err) {
+                return null;
+            }
+
+            fs.writeFileSync(tmpFilePath, stringifyJson(outputJson, 2));
+            return null;
+        }
+
+        vscode.window.showInputBox({ validateInput: liveUpdateOutput }).then((input) => {});
+    });
+}
+
+function runJmespathInPlace() {
+    const selectedJson = getSelectedJson();
+    const activeSelection = getActiveSelection();
+
+    if (!selectedJson) {
+        vscode.window.showInformationMessage('Selected string is not a valid JSON');
+        return;
+    }
+
+    function liveUpdateOutput(pattern: string) {
+        let outputJson;
+        try {
+            outputJson = jmespath.search(selectedJson, pattern);
+        } catch (err) {
+            return null;
+        }
+
+        vscode.window.activeTextEditor.edit((editBuilder) => {
+            editBuilder.replace(activeSelection, stringifyJson(outputJson, 2));
+        });
+
+        return null;
+    }
+
+    vscode.window.showInputBox({ validateInput: liveUpdateOutput }).then((input) => {
+        // User cancels action
+        if (input === undefined) {
+            vscode.window.activeTextEditor.edit((editBuilder) => {
+                editBuilder.replace(activeSelection, stringifyJson(selectedJson, 2));
+            })
+        }
+    });
+}
+
+function getSelectedJson() {
+    let selectedText = getSelectedText();
+
+    if (!selectedText)
+        selectedText = getActiveDocumentText();
+
+    let selectedJson;
+    try {
+        selectedJson = JSON.parse(selectedText);
+    } catch (err) {
+
+    }
+
+    return selectedJson;
+}
+
+function getSelectedText() {
+    var activeEditor = vscode.window.activeTextEditor;
+    var selection = activeEditor.selection;
+    return activeEditor.document.getText(selection);
+}
+
+function getActiveSelection() {
+    const activeEditor = vscode.window.activeTextEditor;
+    const currentSelection = activeEditor.selection;
+
+    // Todo: when nothing is selected, select whole document
+    return currentSelection;
+}
+
+function getActiveDocumentText() {
+    var editor = vscode.window.activeTextEditor;
+    return editor.document.getText();
+}
+
 function getViewColumn(): ViewColumn {
-	const active = vscode.window.activeTextEditor;
-	if (!active) {
+	const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) {
 		return ViewColumn.One;
 	}
 
-	switch (active.viewColumn) {
+	switch (activeEditor.viewColumn) {
 		case ViewColumn.One:
 			return ViewColumn.Two;
 		case ViewColumn.Two:
 			return ViewColumn.Three;
 	}
 
-	return active.viewColumn;
+	return activeEditor.viewColumn;
+}
+
+function stringifyJson(json: any, spaceAmount: number) {
+    return JSON.stringify(json, null, spaceAmount);
 }
